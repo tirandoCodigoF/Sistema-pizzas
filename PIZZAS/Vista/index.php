d1c9c1f3550e29f0c5a722e14b9139e59e088679
tirandoCodigoF <44149781+tirandoCodigoF@users.noreply.github.com>
2019-05-17 16:14:05 -0500

    index
    Co-Authored-By: tirandocodigof <tirandocodigof@users.noreply.github.com>

---
+++ PIZZAS/Vista/index.php
@@ -0,0 +1,26 @@
+<?php
+
+include "navar/head.php";
+include "navar/navar.php";
+require_once '../Datos/Conne.php';
+?>
+<div class="container">
+    <div class="container">
+        <br>
+        <br>
+        <br>
+        <br>
+       <div class="jumbotron jumbotron-fluid">
+           <div class="container">
+               <h1 class="display-3">Acceso al Login</h1>
+              <!--<p class="lead">Iniciar Sesion</p>
+               <hr class="my-2">
+               <p>More info</p>-->
+               <p class="lead">
+                   <a class="btn btn-primary btn-lg" href="login.php" role="button">login</a>
+               </p>
+           </div>
+       </div> 
+    </div>
+</div>
+<?php include "navar/footer.php"; ?>
